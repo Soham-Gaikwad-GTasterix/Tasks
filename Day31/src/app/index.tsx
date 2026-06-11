a6a0d36f0266c2a1d9e83756354c8bf2dd045097
Soham-Gaikwad-GTasterix <sohamgaikwad@gtasterix.com>
2026-06-11 13:21:53 +0530

    Day 31

---
+++ Day31/src/app/index.tsx
@@ -0,0 +1,72 @@
+import { View, ScrollView } from "react-native";
+
+import ScreenTitle from "../components/ScreenTitle";
+
+import DashboardCard from "../components/DashboardCard";
+
+import CustomButton from "../components/CustomButton";
+
+import Section from "../components/Section";
+
+export default function HomeScreen() {
+  return (
+    <ScrollView
+      contentContainerStyle={{
+        padding: 20
+      }}
+    >
+      <ScreenTitle
+        title="Dashboard"
+      />
+
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between"
+        }}
+      >
+
+        <DashboardCard
+          title="Patients"
+          count="250"
+          color="#3b82f6"
+        />
+
+        <DashboardCard
+          title="Doctors"
+          count="20"
+          color="#3b82f6"
+        />
+
+      </View>
+
+      <Section
+        title="Summary"
+      >
+
+        <DashboardCard
+          title="Appointments"
+          count="200"
+          color="#3b82f6"
+          fullWidth
+        />
+
+        <DashboardCard
+          title="Revenue"
+          count="₹ 50,000"
+          color="#3b82f6"
+          fullWidth
+        />
+
+      </Section>
+
+      <CustomButton
+        title="View Reports"
+        onPress={() =>
+          console.log("View Reports")
+        }
+      />
+
+    </ScrollView>
+  );
+}
